7dc130da519d658153cb2c36b5f1d763c240ae7f
kyawyenaing <kyawyenaing.dev@gmail.com>
2023-06-10 22:03:11 +0630

    fix filament user for prod

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,8 +8,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Filament\Models\Contracts\FilamentUser;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
@@ -42,4 +43,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function canAccessFilament(): bool
+    {
+        return str_ends_with($this->email, '@gmail.com');
+    }
 }
