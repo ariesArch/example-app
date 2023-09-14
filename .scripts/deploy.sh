30f63f861b4c382858a43b0f3e40be7a15009fd2
kyawyenaing <kyawyenaing.dev@gmail.com>
2023-09-14 20:31:54 +0630

    try dynamic branch

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 set -e
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <branch>"
+    exit 1
+fi
 
+BRANCH=$1
 echo "Deployment started ..."
-if [ ! -f "vendor/autoload.php" ]; then
-    composer install --no-progress --no-interaction
-fi
+echo "Deployment started for branch: $BRANCH"
 
-if [ ! -f ".env" ]; then
-    echo "Creating env file for env $APP_ENV"
-    cp .env.staging .env
-else
-    echo "env file exists."
-fi
+
+# if [ ! -f ".env" ]; then
+#     echo "Creating env file for env $APP_ENV"
+#     cp .env.staging .env
+# else
+#     echo "env file exists."
+# fi
 # Enter maintenance mode or return true
 # if already is in maintenance mode
 (php artisan down) || true
@@ -21,7 +25,9 @@ git pull origin staging
 
 # Install composer dependencies
 # composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
-
+if [ ! -f "vendor/autoload.php" ]; then
+    composer install --no-progress --no-interaction
+fi
 # Clear the old cache
 php artisan clear-compiled
 
