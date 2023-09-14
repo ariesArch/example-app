8c14c0620398880a462d6b26205ec9487ebeb525
kyawyenaing <kyawyenaing.dev@gmail.com>
2023-09-14 14:49:05 +0630

    fix entry for copying env

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -28,9 +28,23 @@ if [ ! -f "vendor/autoload.php" ]; then
     composer install --no-progress --no-interaction
 fi
 
+if [ -z "$APP_ENV" ]; then
+    echo "APP_ENV is not set. Please set the APP_ENV environment variable."
+    exit 1
+fi
+
+if [ "$APP_ENV" = "staging" ]; then
+    ENV_FILE=".env.staging"
+elif [ "$APP_ENV" = "production" ]; then
+    ENV_FILE=".env.production"
+else
+    echo "Unknown APP_ENV: $APP_ENV"
+    ENV_FILE=".env"
+fi
+
 if [ ! -f ".env" ]; then
     echo "Creating env file for env $APP_ENV"
-    cp .env.example .env
+    cp "$ENV_FILE"  .env
 else
     echo "env file exists."
 fi
